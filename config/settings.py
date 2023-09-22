a12403191b8f077465343c711f474ad4a4338069
Tossiuss <antonio010602@gmail.com>
2023-09-22 11:17:23 +0600

    .

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['34.118.60.99']
+ALLOWED_HOSTS = ['34.118.60.99', '127.0.0.1']
 
 AUTH_USER_MODEL = 'account.USER'
 
