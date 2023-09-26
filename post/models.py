063f710e6a87937974c104d84472457ceb296766
Tossiuss <antonio010602@gmail.com>
2023-09-26 13:21:09 +0600

    .

--- post/models.py
+++ post/models.py
@@ -3,7 +3,7 @@ from account.models import User
 
 class Posts(models.Model):
     title = models.CharField(max_length=30)
-    file_video = models.FileField(upload_to='videos/')
+    file_video = models.FileField(upload_to='videos/', blank=True)
     description = models.TextField()
     categories = models.CharField(blank=False)
     data_create = models.DateTimeField(auto_now_add=True)
