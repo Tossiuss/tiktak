5f378f311897fc10724b7734c36f2e7d257e9a51
Tossiuss <antonio010602@gmail.com>
2023-09-27 18:09:34 +0600

    .

--- post/models.py
+++ post/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from account.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+
 class Posts(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=50)
     file_video = models.FileField(upload_to='videos/', blank=True)
     description = models.TextField(blank=True)
-    categories = models.CharField(blank=False)
+    categories = models.ManyToManyField(Category)
     data_create = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='post')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
 
