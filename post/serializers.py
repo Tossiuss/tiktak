c0a890b2e4d1fe170b7a6651a07b74823b91b299
Tossiuss <antonio010602@gmail.com>
2023-09-27 19:08:47 +0600

    .

--- post/serializers.py
+++ post/serializers.py
@@ -2,19 +2,20 @@ from rest_framework import serializers
 from .models import Posts
 from rivew.serializers import CommentSerializers
 from rivew.serializers import LikeSerializer
-from account.models import User
+
 
 class PostSerializer(serializers.ModelSerializer):
     comments = CommentSerializers(many=True, read_only=True)
     likes = LikeSerializer(many=True, read_only=True)
-
+    name = serializers.CharField(source='user.name', read_only=True)  
+    avatar = serializers.CharField(source='user.avatar', read_only=True)  
 
     class Meta:
-        model = Posts, User
-        fields = ('__all__', 'name', 'avatar')
-        read_only_fields = ('user') 
+        model = Posts
+        fields = '__all__'
+        read_only_fields = ('user',)
 
     def create(self, validated_data):
-        user = self.context.get('request').user
+        user = self.context['request'].user
         post = Posts.objects.create(user=user, **validated_data)
         return post
