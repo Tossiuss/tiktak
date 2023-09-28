72ef62be4075c0237d6e3a7eda3a8630421aecbb
Tossiuss <antonio010602@gmail.com>
2023-09-28 10:02:04 +0600

    .

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import PostView,PostCategorySearchView
 urlpatterns = [
-    path('api/v1/posts/create/', PostView.as_view({'post': 'create'})),
-    path('api/v1/posts/<int:pk>/', PostView.as_view({'delete': 'destroy','get':'retrieve'})),
-    path('api/v1/posts/',PostView.as_view({'get':'list'})),
-    path('api/v1/posts/search/',PostCategorySearchView.as_view())
+    path('posts/create/', PostView.as_view({'post': 'create'})),
+    path('posts/<int:pk>/', PostView.as_view({'delete': 'destroy','get':'retrieve'})),
+    path('posts/',PostView.as_view({'get':'list'})),
+    path('posts/search/',PostCategorySearchView.as_view())
 ]
 
